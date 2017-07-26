dcfb5eabbaeb8701fb112a97d1a97b4be2584e63
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-07-26 09:58:20 -0700

    hack hour done

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,6 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. 
+//Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, 
+//and "fizzbuzz" in place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -17,6 +19,34 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+//num is a number... you build an array of numbers leading up to that number from 1
+//divide each number by 3 and 5 to idenfity if they're divisible by 3 and 5
+//divisible by 3 = "fizz"
+//divisible by 5 = "buzz"
+//divisible by 3 and 5 = "fizzbuzz"
+
+ary = [];
+answerAry = [];
+
+//create the array of numbers
+for(var i = 1; i <= num; i++){
+    ary.push(i)
+}
+
+//iterate through the array of numbers and divide each by 3 and 5
+for(var j = 0; j < ary.length; j++){
+  //check with an if statement if they're divisible by 3 or 5 or both. Both first.
+  if(ary[j] % 3 === 0 && ary[j] % 5 === 0){
+    answerAry.push('fizzbuzz')
+  } else if(ary[j] % 3 === 0) {
+    answerAry.push('fizz')
+  } else if(ary[j] % 5 === 0){
+    answerAry.push('buzz')
+  } else {
+    answerAry.push(ary[j])
+  }
+}
+return answerAry
 
 }
 
