8794a7a7f637085209c1b4fc9304f11e7a64f3ff
alexhong742 <alexhong742@users.noreply.github.com>
2017-07-27 10:15:21 -0700

    imcomplete stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -10,12 +10,37 @@
   ****
  *****
 ******
+/* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
+ The staircase must climb up from left to right. The last line should only consist of asterisks,
+ without any leading/trailing spaces.
+ 
+ For example:     
+ drawStairs(6) ->          
+     *
+    **
+   ***
+  ****
+ *****
+******
 
 */
 
-function drawStairs(n) {
 
+function drawStairs(n) {
+    var arr = ["*"]
+    var space = [" "]
+    var count = 0
+for(var i = 0; i < n-1; i++){
+  space.push(" ")
+ 
+}
+for(var i = n; i > 0; i--){
+  console.log(space.concat(arr))
+  arr.push("*")
+  space.pop()
 }
 
-
+    
+}
+drawStairs(4)
 module.exports = drawStairs;
