5bb1fd795d147305ccedee09d98f4475675e37a1
David Sally <david@Davids-MacBook-Pro.local>
2017-07-27 10:03:10 -0700

    draw stairs now complete

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -15,46 +15,47 @@
 
 function drawStairs(n) {
 
-	// **********************
-	// ** Input Validation **
-	// **********************
+	// ********************************************
+	// ** Input Validation - return false if bad **
+	// ********************************************
 
 	// check for objects
-	if (typeof n === 'object') {
-		printError();
-	}
+	if (typeof n === 'object') { return printError(); }
 
-	// check for bad numbers
-	else if (n < 1 || n > 100) {
-		printError();
-	}
-
+	// check for numbers out of range
+	else if (n < 1 || n > 100) { return printError(); }
 
 	// check for strings
-	else if (typeof n === 'string') {
-		printError();
-	}
-	if (n > 100) {
-		return false;
-	}
+	else if (typeof n === 'string') { return printError(); }
 
+	// *****************
+	// ** Run program **
+	// *****************
+	let space = ' ';
+	let stair = '*';
 
-
-	// draw stairs, counting up from 1 stair to n
-	// pad stairs with spaces, for spaces - i	
+	// loop through numbers
 	for (let i = 1; i <= n; i++) {
 		let current = '';
+
+		// assign number of spaces
 		let numSpaces = n - i;
 
+		// add to space padding
 		for (let j = 0; j < numSpaces; j++) {
-			current += " ";
+			current += space;
 		}
+
+		// add stairss
 		for (let j = 0; j < i; j++) {
-			current += "*";
+			current += stair;
 		}
+
+		// print out current level of stairs
 		console.log(current);
 	}
 
+	// this is used to log invalid parameters and return false
 	function printError() {
 		console.log("[Error] invalid input: " + n);
 		console.log("Must be num between 1 -> 100");
@@ -63,45 +64,52 @@ function drawStairs(n) {
 
 }
 
-console.log("normal (4): ");
-drawStairs(4);
-console.log("normal: (1): ");
-drawStairs(1);
+// ***********
+// test cases: 
+// ***********
 
-console.log("0: ");
-drawStairs(0);
-console.log("null: "); 
-drawStairs(null);
+//// valid
+// console.log("100: ");
+// drawStairs(100);
 
-console.log("undef: ");
-drawStairs();
+// console.log("normal (4): ");
+// drawStairs(4);
+// console.log("normal: (1): ");
+// drawStairs(1);
 
-console.log("[]: ");
-drawStairs([]);
-console.log("[1,2]: ");
-drawStairs([1,2]);
 
-console.log("{}");
-drawStairs({});
-console.log("{a: 'b'}: ");
-drawStairs({a: 'b'});
+// // invalid
+// console.log("0: ");
+// drawStairs(0);
+// console.log("null: "); 
+// drawStairs(null);
 
-console.log("string ('7'):  ");
-drawStairs('7');
-console.log("string ('seven'): ");
-drawStairs('seven');
+// console.log("undef: ");
+// drawStairs();
 
-console.log("100: ");
-drawStairs(100);
+// console.log("[]: ");
+// drawStairs([]);
+// console.log("[1,2]: ");
+// drawStairs([1,2]);
 
-console.log("101: ");
-drawStairs(101);
+// console.log("{}");
+// drawStairs({});
+// console.log("{a: 'b'}: ");
+// drawStairs({a: 'b'});
 
-console.log("-1: ");
-drawStairs(-1);
+// console.log("string ('7'):  ");
+// drawStairs('7');
+// console.log("string ('seven'): ");
+// drawStairs('seven');
 
-console.log("-100: " );
-drawStairs(-100);
+// console.log("101: ");
+// drawStairs(101);
+
+// console.log("-1: ");
+// drawStairs(-1);
+
+// console.log("-100: " );
+// drawStairs(-100);
 
 
 module.exports = drawStairs;
