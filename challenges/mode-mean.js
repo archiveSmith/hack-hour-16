d2432dbb7721a2f96af1e07c78c6c2c9016dacc6
Mila Hose <mila.hose@gmail.com>
2017-07-29 10:14:00 -0700

    Add mode-mean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
+  var num = 0;
+  
+	for (var i = 0; i < array.length; i++) {
+    num += array[i];
+  }
+  
+  var mean = Math.floor(num / array.length);
+  
+  var mode = {};
+  
+  for (var i = 0; i < array.length; i++) {
+     if (mode[array[i]]) {
+       mode[array[i]]++;
+     } else {
+       mode[array[i]] = 1;
+     }
+  }
+  
+	mode = Object.values(mode).reduce(function(prev, cur) {
+    return prev > cur ? prev : cur;
+  });
 
+  
+  console.log(mode)
+  return mean === mode;
 }
 
 module.exports = modemean;
