7d98fc11786a85d04de86790ed8f5a86cbc2e682
Jon Roach <roach.jc@gmail.com>
2017-07-29 10:19:21 -0700

    mode-mean 1st attempt

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,21 @@
 
 
 function modemean(array) {
+  const count = array.reduce((count, num) => {
+    if (count[num]) count[num]++;
+    else count[num] = 1
+    return count;
+  }, {});
 
+  const mode = Object.keys(count).reduce((mostRepeated, current) => {
+    if (count[current] > count[mostRepeated]) {
+      mostRepeated = current;
+    }
+    return mostRepeated;
+  }, array[0]);
+
+  const mean = Math.floor(array.reduce((sum, num) => sum + num) / array.length);
+  return mean.toString() === mode;
 }
 
 module.exports = modemean;
