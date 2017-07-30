cc307b26990cbad3ac11e67b041c037c94db04fe
JTak <jasonytak@gmail.com>
2017-07-29 18:13:05 -0700

    Challenges updated

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,45 @@
  *
  */
 
+ function modemean(array) {
+   let sum = array.reduce(function(a, b) {
+     return a + b;
+   });
+   const length = array.length
+   let mean = sum / length;
 
-function modemean(array) {
-
-}
+   var countObj = {};
+   for (let i = 0; i < array.length; i++) {
+     var current = array[i];
+     if (!countObj[current]) {
+       countObj[current] = 1;
+     } else {
+       countObj[current] = countObj[current] + 1;
+     }
+   }
+   let counter = 0;
+   let arr1 = [];
+   for (let key in countObj) {
+     if (countObj[key] > counter) {
+       counter = countObj[key];
+     }
+   }
+   for (let key in countObj) {
+     if (countObj[key] == counter) {
+       arr1.push(key);
+     }
+   }
+   var realArr = arr1.map(function(x) {
+     return parseInt(x, 10);
+   })
+   var mode = realArr.reduce(function(a, b) {
+     return Math.max(a, b);
+   })
+   if (mean === mode) {
+     return true;
+   } else {
+     return false;
+   }
+ }
 
 module.exports = modemean;
