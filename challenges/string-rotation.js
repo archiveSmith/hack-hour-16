6b26a6c0e555f58ade6eb70beddfb6d963d5543b
wards <wardaaa@me.com>
2017-08-05 09:59:53 -0700

    string-rotation challenge complete

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -10,13 +10,29 @@
  *              stringRotation("hello", "he") -> false
  *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
  */
-
 function isSubstring(s1, s2) {
-  return s1.indexOf(s2) >= 0;
+    return s1.indexOf(s2) >= 0;
 }
 
 function stringRotation(s1, s2) {
+    // compare the first half of s1 to the last half of s2
+    // access first half thru substring(), start: str.len/2, end: str.len
+    const left = s1.substring(s1.length / 2);
+    // console.log(left)
 
+    // access last rotated half via substring() start: 0, end: str.len
+    const right = s2.substring(0, s2.length);
+    // console.log(right)
+
+    // check if s2 half is found in index of s1 -> call isSubstring
+    // console.log('isSubstring:' + isSubstring(right, left));
+    return isSubstring(right, left);
 }
 
-module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+console.log(stringRotation('hello', 'hello'));
+console.log(stringRotation('hello', 'llohe'));
+console.log(stringRotation('hello', 'he'));
+console.log(stringRotation('hello', 'ollhe'));
+
+
+module.exports = { isSubstring, stringRotation };
