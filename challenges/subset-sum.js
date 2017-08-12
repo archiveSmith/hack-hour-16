b1a3dca0256ebf9dc4006946a557e0668c06ed6d
Jon Roach <roach.jc@gmail.com>
2017-08-12 09:44:15 -0700

    bin2dec and subsetsum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,9 @@
  */
 
 function subsetSum(array, target) {
-
+  if (target === 0) return true;
+  if (!array.length) return false;
+  return subsetSum(array.slice(1), target - array[0]) || subsetSum(array.slice(1), target);
 }
 
 module.exports = subsetSum;
