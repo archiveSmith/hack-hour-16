d48d72f60900b8c85566e64d5dd0b7a4f70a3dd2
wards <wardaaa@me.com>
2017-08-12 10:02:51 -0700

    bin to dec challenge

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,35 @@
  */
 
 function binToDec(binary) {
+  // iterate through len of input, each i -> pow of 2, base len-1-i
+  // (11) --> 0 x 2 + 1 = 1 -> 1 x 2 + 1 = 3 - 11->3
 
+
+  let res = 0;
+  const len = binary.length;
+  //   console.log(`len:${len}`);
+
+  for (let i = len - 1; i >= 0; i--) {
+    // console.log(`i:${i}`);
+
+    const j = len - 1 - i;
+    // console.log(`j:${j}`);
+
+    const num = parseInt(binary.charAt(i));
+    // console.log(`num:${num}`);
+
+    const addTwo = num * Math.pow(2, j);
+    res += addTwo;
+  }
+
+  return res;
 }
+
+console.log(binToDec('0'));
+console.log(binToDec('11'));
+console.log(binToDec('100'));
+console.log(binToDec('101'));
+console.log(binToDec('0101'));
+
 
 module.exports = binToDec;
