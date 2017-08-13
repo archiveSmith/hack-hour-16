ec0379905a9cf6dd981147256ec4f9d19787b632
William <he.william@yahoo.com>
2017-08-13 10:52:40 -0700

    didnt finish

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,38 @@
  */
 
 function binToDec(binary) {
-
+    let number = 0;
+    binary = binary.split(""); //['1','1']
+    let pow = binary.length - 1
+    newNumbers = binary.map(function (element) {
+        return Number(element)
+    })
+    for (var i = 0; i < binary.length; i++) {
+        number = number + binary[i] * Math.pow(2, pow)
+        pow--
+    }
+    return number
+    //loop through the array
+    //grab each number and mulitply by 2 ^ pow 
+    // decrement pow
 }
+
+function decToBin(dec) {
+    var binary = ""
+    something = 2
+    for (var i = something; i >= 0; i--) {
+        if (dec >= Math.pow(2, something)) {
+            
+            binary = binary + "1"
+            dec = dec - Math.pow(2, something)
+            something--
+        } else {
+            binary = binary + "0"
+        }
+    }
+    return binary
+}
+
+console.log(decToBin(5))
 
 module.exports = binToDec;
--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,39 @@
  */
 
 function romanNumeral(n) {
+  var reference = {
+    "1": "I",
+    "4": "IV",
+    "5": "V",
+    "9": "IX",
+    "10": "X",
+    "40": "XL",
+    "50": "L",
+    "90": "XC",
+    "100": "C",
+    "400": "CD",
+    "500": "D",
+    "900": "CM",
+    "1000": "M",
+  }
+  var result = "";
+  var total = 0;
+  
+  
+  for(key in reference) {
 
+    if(n > key) {
+      if(n === Number(key)) {
+        total = total + reference[key]
+        result = result + key
+      }
+      while(total !== n) {
+        total++
+        result=result+"I"
+      }
+    }
+  }
+  return result
 }
 
 module.exports = romanNumeral;
