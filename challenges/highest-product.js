1e07933c172fb6ff681a93f34ba965c5df29e87a
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-08-16 09:45:21 -0700

    finished highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,30 @@
  */
 
 function highestProduct(array) {
-
+   
+ sortedAry = array.sort(function(a, b){return a - b});
+ console.log(sortedAry)
+ 
+ productNeg = 1;
+ 
+ for(var i = 0; i < 3; i++){
+  productNeg = productNeg * sortedAry[i]
+ }
+ //console.log(productNeg) 
+  
+  productPos = 1;
+  
+  for(var j = sortedAry.length-1; j > sortedAry.length - 4; j--){
+    productPos = productPos * sortedAry [j]
+  }
+  
+  //console.log(productPos)
+  
+  if(productNeg > productPos){
+    return productNeg
+  } else {
+    return productPos
+  }
 }
 
 
