e7ccbdc8ca2cb12d066d6aaf446d00bf7d83bc86
Clifford <deeharvey06@yahoo.com>
2017-08-18 09:25:50 -0700

    challenge 13

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,26 @@
  */
 
 function highestProduct(array) {
+  const arr = [];
+  let highest = array[0];
+  let first;
+  array.forEach((ele) => {
+    if (ele > highest) {
+      arr.push(ele);
+      const slices = array.indexOf(ele);
+      array.splice(slices, 1);
+    }
+  });
 
+  array.forEach((ele) => {
+    console.log(highest)
+    if (ele > highest) {
+      highest = ele;
+    }
+  });
+  // console.log(arr);?
+  // console.log(array);
 }
 
-
+console.log(highestProduct([6, 3, 5, 8, 1, 3]));
 module.exports = highestProduct;
