57f7e2aaaa54098e9fdd675265ea6142431831c4
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-08-26 09:51:20 -0700

    hack hour done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,39 @@
  */
 
 function permPalin(str) {
-	
+    
+     
+      var arr = (str + '').split(''),
+      permutations = [];   
+    
+      function swap(a, b)
+      {
+        var tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+      }
+    
+      function generate(n) {
+        if (n == 1) {
+          permutations.push(arr.join());
+        } else {
+          for (var i = 0; i != n; ++i) {
+            generate(n - 1);
+            swap(n % 2 ? 0 : i, n - 1);
+          }
+        }
+      }
+    
+      generate(arr.length);
+
+for(var i = 0; i < permutations.length; i++){
+   if(permutations[i].split(',').join('') === permutations[i].split(',').reverse().join('')) {
+     return true
+   }
+}
+return false
+  
+   
 }
 
 module.exports = permPalin;
