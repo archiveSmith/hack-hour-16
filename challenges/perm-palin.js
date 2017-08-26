230df863b4cb7003b469cb92dbfaf527533da2be
Jon Roach <roach.jc@gmail.com>
2017-08-26 09:37:40 -0700

    is palindrome done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,16 @@
  */
 
 function permPalin(str) {
-	
+  const charCount = {};
+  for (let i = 0; i < str.length; i += 1) {
+    if (charCount[str[i]]) charCount[str[i]] += 1;
+    else charCount[str[i]] = 1;
+  }
+  let oddCounts = 0;
+  Object.keys(charCount).forEach((count) => {
+    if (charCount[count] % 2 !== 0) oddCounts += 1;
+  });
+  return oddCounts <= 1;
 }
 
 module.exports = permPalin;
