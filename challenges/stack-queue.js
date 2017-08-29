f2f9dff5cdd2f77baccc043d6f25d7c52d1f1f47
Jon Roach <roach.jc@gmail.com>
2017-08-29 10:01:23 -0700

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,21 @@
 
 
 function Stack() {
-
+  this.storage = {};
+  this.length = 0;
 }
 
+Stack.prototype.push = (value) => {
+  this.storage[this.length] = value;
+  this.length += 1;
+};
+
+Stack.prototype.pop = () => {
+  const popped = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length -= 1;
+  return popped;
+};
 
 /**
 * Queue Class
@@ -14,7 +26,22 @@ function Stack() {
 
 
 function Queue() {
-
+  this.stack1 = new Stack();
+  this.stack2 = new Stack();
 }
+
+Queue.prototype.enqueue = (item) => {
+  this.stack1.push(item);
+};
+
+Queue.prototype.dequeue = () => {
+  if (!this.stack2.length) {
+    if (!this.stack1.length) return null;
+    while (this.stack1.length) {
+      this.stack2.push(this.stack1.pop);
+    }
+  }
+  return this.stack2.pop();
+};
 
 module.exports = {Stack: Stack, Queue: Queue};
