d990c369e2707ba7b85324efb96610190fa84892
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-08-30 09:51:16 -0700

    two sum done

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -6,6 +6,9 @@ function twoSum(arr, n) {
 
     for (var i = 0; i < arr.length; i++) {
         for (var j = 0; j < arr.length; j++) {
+          if(i === j){
+              continue;
+          }
             if (arr[i] + arr[j] === n) {
                 return true
             }
