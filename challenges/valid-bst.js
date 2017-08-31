c3e3b860a597336b18987ad336f044d1c02e82f6
Jon Roach <roach.jc@gmail.com>
2017-08-31 14:06:54 -0700

    valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -13,7 +13,10 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
-
+  if (!tree) return true;
+  if (tree.left !== null && tree.left.value > tree.value) return false;
+  if (tree.right !== null && tree.right.value < tree.value) return false;
+  return validBST(tree.left) && validBST(tree.right);
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
