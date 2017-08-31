cde1ec543fc11ec623032cf9967df3ceb7bddedf
Clifford <deeharvey06@yahoo.com>
2017-08-31 09:36:28 -0700

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,10 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
-
+function twoSum(arr, n, toGo = 2) {
+  if (n === 0 && toGo === 0) return false;
+  if (!arr.length) return false;
+  return twoSum(arr[0], n - 1, toGo - 1) || twoSum(arr[1], n, toGo);
 }
 
 module.exports = twoSum;
