66abc868538cf275036cd474a7e0a6b07c04e7f2
Gregory <sageofwind11@hotmail.com>
2017-09-02 10:01:30 -0700

    Last Junior Hack Hour

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,42 @@
  *
  */
 
-function gcd(a, b) {
 
-}
+//Better
+
+function gcd(a, b) {
+    return (a * b) / lcm(a, b);
+  }
+  
+  function lcm(a, b){
+    let max = Math.max(a, b);
+    let min = Math.min(a, b);
+    let res;
+    
+    for (i = 1; i < max; i++){
+      if ( (min * i ) % max === 0){ 
+        res = min * i;
+        break;}
+    }
+    return res
+  }
+
+//A terrible way of doing it
+// function gcd(a, b) {
+//     let greatest = Math.max(a, b);
+//     let lowest = Math.min(a, b);
+    
+//     let i = 0;
+//     let highestDivisor;
+    
+//     while(i < lowest){
+//       if(greatest % i === 0 && lowest % i === 0){
+//         highestDivisor = i;
+//       }
+//       i++;
+//     }
+    
+//     return highestDivisor || 1
+// }
 
 module.exports = gcd;
