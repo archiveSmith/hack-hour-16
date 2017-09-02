75baabc33079dd013f3bae39c0f351ec2af55a42
David Sally <david@Davids-MacBook-Pro.local>
2017-09-02 09:15:50 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,16 @@
  */
 
 function gcd(a, b) {
+  
+  // start at smallest number
+    // count down from smallest, checking when / if both are divisible, if so, return
+  const min = Math.min(a, b);
 
+  for (let i = min; i > 0; i--) {
+    if (a % i === 0 && b % i === 0) return i;
+  }
+  return 1;
 }
+
 
 module.exports = gcd;
