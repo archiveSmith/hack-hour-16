677246742b5102fb2cee441d48f30c19f371fb5c
Grant Kang <g1k05j@gmail.com>
2017-09-02 09:15:43 -0700

    gcd SENIORS

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,19 @@
  */
 
 function gcd(a, b) {
+  let div1 = [];
+  let div2 = [];
 
+  for (let i = 1; i < Math.max(a, b); i++) {
+    if (a % i === 0) div1.push(i)
+    if (b % i === 0) div2.push(i)
+  }
+  console.log(div1);
+  console.log(div2);
+  return div1.reduce((gcd, num) => {
+    if (div2.indexOf(num) > -1 && num > gcd) gcd = num
+    return gcd
+  })
 }
-
+console.log(gcd(10,8))
 module.exports = gcd;
