b73c8cf621c14bcee1fd76436e9e40c2c997bf1c
William <he.william@yahoo.com>
2017-09-03 16:25:39 -0700

    finished gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,6 +8,30 @@
  */
 
 function gcd(a, b) {
+    let resultsofa = [];
+    let resultsofb = [];
+    let common = [];
+
+    for(var i = 0; i< a; i++) {
+        if(a % i === 0) {
+            resultsofa.push(i)
+        }
+    }
+    
+    for(var j = 0; j< b; j++) {
+      if(b % j === 0) {
+        resultsofb.push(j)
+      }
+    }
+    
+    
+    for(var k = 0; k < resultsofa.length; k ++ ) {
+      if(resultsofb.indexOf(resultsofa[k]) !== -1) {
+        common.push(resultsofa[k])
+      }
+    }
+    
+    return Math.max(...common)
 
 }
 
