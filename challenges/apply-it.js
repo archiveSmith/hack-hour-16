978926a7a4a144a7d60b1ea3fec6a4253c8b0519
alexhong742 <alexhong742@users.noreply.github.com>
2017-09-11 09:55:09 -0700

    applyit complete

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -25,8 +25,16 @@
  *  DO NOT USE THE BUILT IN APPLY METHOD OR THE SPREAD OPERATOR
  */
 
+const alex = function(name,age,location) {
+  return name + " is " + age + " and he lives in " + location;
+};
 function applyIt(func, args) {
-
+  if(!args.length){return 'info not available'}
+  if(args.length === 1) {return args[0] + " is a beast"}
+  if(args.length === 2 && typeof(args[1]) === 'number'){return args[0] + " is " + args[1] + " years old"}
+  if(args.length > 2 && typeof(args[1]) === 'number' && typeof(args[2]) === 'string'){return args[0] + " is " + args[1] + " years old and he lives in " + args[2]}
+  return 'Error: these types of arguments dont work'
 }
+// const alexFunc = applyIt(alex, ["alex", 24, "Fullerton"]);
 
 module.exports = applyIt;
