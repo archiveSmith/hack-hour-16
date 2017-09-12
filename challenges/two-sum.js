818dd5fd8d1f532e4b8d830f15b3e68230f57560
William <he.william@yahoo.com>
2017-09-12 09:39:50 -0700

    finished two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,20 @@
  */
 
 function twoSum(arr, n) {
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] > n) {
+            arr.splice(i, 1)
+        }
+    }
 
+    for(var j = 0; j < arr.length; j++) {
+        let need= n - arr[j]
+        if(arr.indexOf(need) !== -1) {
+            return true 
+        } else {
+            continue
+        }
+    }
 }
 
 module.exports = twoSum;
