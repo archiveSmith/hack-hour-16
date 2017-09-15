0d30f909fab4fcf190830db5aa46f89e42f3cf30
Jon Roach <roach.jc@gmail.com>
2017-09-15 08:54:40 -0700

    superbalanced

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -12,9 +12,48 @@ function BinaryTree(value) {
   this.left = null;
   this.right = null;
 }
+BinaryTree.prototype.add = function(value) {
+  const leaf = new BinaryTree(value);
+  if (value > this.value) {
+    if (!this.right) this.right = leaf;
+    else this.right.add(value);
+  } else {
+    if (!this.left) this.left = leaf;
+    else this.left.add(value);
+  }
+};
+
+const bst = new BinaryTree();
+bst.push(3);
+bst.push(2);
+bst.push(4);
+bst.push(1);
+bst.push(5);
+
+function height(tree) {
+  if (!tree) return 0;
+  return 1 + Math.max(height(tree.left), height(tree.right));
+}
+
+function maxDepth(node) {
+  if (!node) return 0
+  return Math.max(maxDepth(node.right), maxDepth(node.left)) + 1;
+}
+
+function minDepth(node) {
+  if (!node) return 0;
+  return Math.min(minDepth(node.right), minDepth(node.left)) + 1;
+}
 
 function superbalanced(tree) {
-
+  const rightHeight = height(tree.right);
+  const leftHeight = height(tree.left);
+  const maxSide = Math.max(rightHeight, leftHeight);
+  const minSide = Math.min(rightHeight, leftHeight);
+  const min = minDepth(tree);
+  const max = maxDepth(tree);
+  return (maxSide - minSide <= 1) && (max - min <= 1);
 }
+
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
