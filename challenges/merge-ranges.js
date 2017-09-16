7c17d977a6601542a553ee1e335242cfe172e8b6
Grant Kang <g1k05j@gmail.com>
2017-09-16 09:28:56 -0700

    Merge-Ranges Completed

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,27 @@
 
 
 function mergeRanges(array) {
+  let mergedTimes = []
+  let sortedTimes = array.sort((a, b) => {
+    return a.reduce((acc, el) => acc+el) - b.reduce((acc, el) => acc + el)
+  })
+  console.log(sortedTimes)
+  for (let i = 0; i < sortedTimes.length; i++) {
+    const startTime = sortedTimes[i][0]
+    let endTime = sortedTimes[i][1]
+    for (let j = i+1; j < sortedTimes.length; j++) {
+      if (endTime >= sortedTimes[j][0]) {
+        endTime = sortedTimes[j][1]
+        sortedTimes.splice(j, 1)
+        console.log('this is endTime: ', endTime)
+      }
+      mergedTimes[i] = [startTime, endTime]
+    }
+  }
 
+  return mergedTimes
 }
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+console.log(mergeRanges(times))// [[0, 1], [3, 8], [9, 12]]
 
 module.exports = mergeRanges;
