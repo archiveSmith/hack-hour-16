6b18e1b0edc567a5e9921884255ef9f841fc44a6
Gregory <sageofwind11@hotmail.com>
2017-09-18 09:36:49 -0700

    Message

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,24 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
+  let valArray = [];
+  
+  while(!valArray.includes(head.value) && head.next){
+    valArray.push(head.value)
+    head = head.next;
+    if(head.next === null) return false;
+  }
 
+  return true;
 }
 
+ var node1 = new Node('1');
+ var node2 = node1.next = new Node('2');
+ var node3 = node2.next = new Node('3');
+ var node4 = node3.next = new Node('4');
+ var node5 = node4.next = new Node('5');
+
+ hasCycle(node1); // => false
+ node5.next = node2;
+ hasCycle(node1); //=> true
 module.exports = {Node: Node, hasCycle: hasCycle}
