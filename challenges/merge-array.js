7ec2483e010db60ce9dbd309002edf4fd27ab9c4
Balal Zuhair <balalzuhair@Balals-MacBook-Pro.local>
2017-09-19 09:22:20 -0700

    need to look over

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -34,4 +34,38 @@
  //   return result;
  }
 
+ const mergeArrays = (array) => {
+   // sort array
+   array.sort((a,b) => {
+     // if first item in both arrays is the same, sort them according to last item
+     // otherwise, sort them according to first item
+     if (a[0] === b[0]) return a[1] - b[1];
+     return a[0] - b[0];
+   });
+
+   for (let i = 1; i < array.length i+= 1) {
+     const curr = array[i];
+     const prev = array[i - 1];
+     const next = array[i + 1];
+
+     // check for encapsulated times (looking one ahead)
+     if (next && next[0] >= prev[0] && next[next.length -1] <= curr[curr.length - 1]) {
+       array.splice(i + 1], 1);
+     }
+
+     // if first item in curr <= last item in prev, merge them
+     if(curr[0] <= prev([rev.length -1]) {
+       const merged = prev.concat(curr);
+       // sort merged and splice out all values except for first and last
+       merged.sort((a,b) => a - b).splice(1, merged.length -2);
+       // splice out two overlapping arrays and insert merged array
+       array.splice(i - 1, 2, merged);
+       // decrement i (since we replaced two overlapping arrays with one merged array)
+       i -= 1;
+     }
+   }
+   return array;
+ }
+
+
 module.exports = mergeArrays;
