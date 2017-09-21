d2a3719c216b40091f16a373fa6be00b9d54ab85
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-09-21 09:26:25 -0700

    anagrams done

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -14,6 +14,35 @@
 
 function anagrams(string) {
 
+  function permutationArr(aryStr) {
+    
+      var arr = (aryStr + '').split(',')
+      var permutations = [];   
+      
+    
+      function swap(a, b) {
+        var tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+      }
+    
+      function generate(n) {
+        if (n == 1) {
+          permutations.push(arr.join(''));
+        } else {
+          for (var i = 0; i != n; ++i) {
+            generate(n - 1);
+            swap(n % 2 ? 0 : i, n - 1);
+          }
+        }
+      }
+    
+      generate(arr.length);
+      return permutations;
+    }    
+    
+    return permutationArr(string.split(''))
+
 }
 
 module.exports = anagrams;
