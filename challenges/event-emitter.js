6fad439770d968205bc679a81ba3cc30d21f240e
Jon Roach <roach.jc@gmail.com>
2017-09-23 09:33:21 -0700

    ee

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,30 @@
  */
 
 function EventEmitter() {
-
+  this.handlers = {};
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  if (this.handlers[funcName]) this.handlers[funcName].push(func);
+  else this.handlers[funcName] = [func];
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  if (funcName in this.handlers) {
+    this.handlers[funcName].forEach(func => func(...args));
+  }
 };
+
+var instance = new EventEmitter();
+var counter = 1;
+
+instance.on('increment', function(a, b) {
+  counter += a + b;
+}); // counter should be 0
+
+instance.trigger('increment', 1, 2); // counter should be 1
+console.log(counter);
+instance.trigger('increment', 10, 20); // counter should be 2
+console.log(counter)
 
 module.exports = EventEmitter;
