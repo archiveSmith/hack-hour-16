5770195b27c71c9a8dfc409f7195eb29fa030d0f
Balal Zuhair <balalzuhair@Balals-MacBook-Pro.local>
2017-09-25 09:51:58 -0700

    stil in works

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -34,3 +34,62 @@ EventEmitter.prototype.trigger = function(funcName, ...args) {
 };
 
 module.exports = EventEmitter;
+
+
+
+
+//
+// function EventEmitter() {
+//   this.storage = [false];
+//
+// }
+//
+// EventEmitter.prototype.on = function(funcName, func) {
+//   this.storage[0] = true;
+//   let newFunc = function() { func()};
+//   this.storage.push(newFunc)
+//   console.log(`on counter: ${counter}`)
+//   return `on counter: ${counter}`
+// };
+//
+// EventEmitter.prototype.trigger = function(funcName, ...args) {
+//   if(this.storage[0]) this.storage[1]()
+//   return `counter: ${counter}`
+//
+// };
+//
+// var instance = new EventEmitter();
+// var counter = 0;
+//
+// instance.on('increment', function() {
+//   counter++;
+// }); // counter should be 0
+//
+// instance.trigger('increment'); // counter should be 1
+// // instance.trigger('increment'); // counter should be 2
+//
+
+
+
+
+
+
+
+
+// function EventEmitter() {
+//   // dfine 'on' method that takes an event and a callback as parameters
+//   on(event, callback) {
+//     // if this[event] is undefined, initialize it as an array with callback inside
+//     if (!this[event]) this[event] = [callback];
+//     // else push callback into this[event]
+//     else this[event].push(callback);
+//   }
+//
+//   // difine 'trigger' method that takes event as 1st parameters
+//   // use rest operator to capture all remaining arguments
+//   trigger(event, ...args) {
+//     // if this[event] exists, loop thourgh this[event] using forEach and invoke every callback inside
+//     // use
+//     if
+//   }
+// }
