195fd14fdced6f488b5f16eed9ea7de68cf4c1ff
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-09-26 09:04:07 -0700

    event emitter changes

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -38,6 +38,14 @@ function EventEmitter() {
       this.func(args)
     }
   
+
+    var instance = new EventEmitter();
+    var counter = 0;
+    instance.on('increment', function() {
+      counter++;
+    }); // counter should be 0
+    instance.trigger('increment'); // counter should be 1
+    instance.trigger('increment'); // counter should be 2
   
   };
 module.exports = EventEmitter;
