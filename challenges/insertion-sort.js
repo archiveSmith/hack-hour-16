457650c93b48fca3a729e3ed9e01c8e47aa55781
Grant Kang <g1k05j@gmail.com>
2017-09-26 09:12:23 -0700

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -6,6 +6,16 @@
 
 function insertionSort(array) {
 
+  for (let i = 0; i < array.length; i++) {
+    let temp = array[i]
+    var j = i - 1
+    while (j >= 0 && temp < array[j]) {
+      array[j+1] = array[j]
+      j--
+    }
+    array[j + 1] = temp;
+  }
+  return array
 }
-
+console.log(insertionSort([6,5,4,3,2,1]))
 module.exports = insertionSort;
