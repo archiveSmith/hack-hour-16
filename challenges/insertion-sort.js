7e65d23b521f54e960d7658f07a87c008435f704
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-09-26 09:47:49 -0700

    insertion sort done

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -4,8 +4,32 @@
 
 
 
-function insertionSort(array) {
-
-}
+function insertionSort(array){
+    
+    for(var i = 0; i < array.length; i++){
+      // console.log(array[i])
+      if(array[i-1] === undefined) {
+        continue;
+      }
+      
+      if(array[i-1] < array[i]){
+        continue;
+      } else {
+        j = i;
+        temp = array[i]
+        
+        while(array[j] < array[j-1]){
+          temp2 = array[j]
+          array[j] = array[j-1]
+          array[j-1] = temp2 
+        j--
+        }
+        
+        }
+        // console.log(array)
+    }
+    // array[j] = array[i]
+  return array
+  }
 
 module.exports = insertionSort;
