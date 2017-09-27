86db657bbf2b83bb71b92dfb50e010faf0db0495
Grant Kang <g1k05j@gmail.com>
2017-09-27 09:44:14 -0700

    unique-number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,26 @@
  * Complete the challenge in O(1) space
  *
  */
+
 function uniqueNumber(array) {
-
+  // let expectedSum = 0
+  // let sum = 0
+  // for (let i = 0; i < array.length; i++) {
+  //   console.log(expectedSum, sum)
+  //   expectedSum += array[i] * 2
+  //   sum += array[i]
+  // }
+  //
+  // console.log(expectedSum, sum)
+  return array.filter(num => {
+    return array.indexOf(num) == array.lastIndexOf(num)
+  })
 }
-
+// console.log(uniqueNumber([6,4,6,3,3])) //4
+// console.log(uniqueNumber([1,2,1,3,3])) //2
+console.log(uniqueNumber([7,2,7,5,2])) //5
 module.exports = uniqueNumber;
+
+// 1
+// 3
+// 2
