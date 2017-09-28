5b37c71d40aa026fa2e5fee2d007bded6d856c74
Gordon Yu <gordonyu@Gordons-MacBook-Pro.local>
2017-09-28 09:32:36 -0700

    get all products done

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,45 @@
  */
 
 function getAllProducts(array) {
-
+    array.join('')
+    var arr = (array + '').split(','),
+    permutations = [];   
+    function swap(a, b)
+    {
+      var tmp = arr[a];
+      arr[a] = arr[b];
+      arr[b] = tmp;
+    }
+    function generate(n) {
+      if (n == 1) {
+        permutations.push(arr.join(''));
+      } else {
+        for (var i = 0; i != n; ++i) {
+          generate(n - 1);
+          swap(n % 2 ? 0 : i, n - 1);
+        }
+      }
+    }
+    generate(arr.length);
+    // console.log(permutations);
+    var products = [];  
+    
+    for(var i = 0; i < permutations.length; i++) {
+      product = 1;
+      findProduct = permutations[i].split('')
+      
+      for(var j = 1; j < findProduct.length; j++) {
+        // console.log(findProduct[j])
+        product = product * findProduct[j]
+      }
+    // console.log(product)
+    products.push(product)
+    }
+    
+  var unique = products.filter(function(itm, i, a) {
+      return i == products.indexOf(itm);
+  });
+  return unique
 }
 
 module.exports = getAllProducts;
