97399bd58366a64574aca478a55676d43d1c0000
Jon Roach <roach.jc@gmail.com>
2017-09-28 09:13:51 -0700

    unique number tbc

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -2,7 +2,7 @@
  * Write a function that will take an array of integers, all of which will appear exactly twice,
  * except for one integer that will appear exactly once. Return the integer that appears once.
  *
- * uniqueNumber([1,2,1,3,3]); -> 2
+  -> 2
  *
  * BONUS:
  * Complete the challenge in O(n) time
@@ -10,7 +10,12 @@
  *
  */
 function uniqueNumber(array) {
-
+  array = array.sort();
+  for (let i = 0; i < array.length; i += 2) {
+    if (array[i] !== array[i + 1]) return array[i];
+  }
 }
+
+console.log(uniqueNumber([1,2,1,3,3]));
 
 module.exports = uniqueNumber;
