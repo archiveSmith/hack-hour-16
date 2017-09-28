a652267d7d4088d79b2049f7634cbdde5e8483e0
Jon Roach <roach.jc@gmail.com>
2017-09-28 09:33:42 -0700

    get high prod

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,18 @@
  */
 
 function getAllProducts(array) {
-
+  const result = [];
+  let product = 1;
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length; j += 1) {
+      if (i !== j) product *= array[j];
+    }
+    result.push(product);
+    product = 1;
+  }
+  return result;
 }
+
+console.log(getAllProducts([1, 7, 3, 4]));
 
 module.exports = getAllProducts;
