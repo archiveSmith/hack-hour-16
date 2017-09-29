5eb33b99edd5a4e260e1ef5cfeac43b76797338b
Grant Kang <g1k05j@gmail.com>
2017-09-29 09:32:06 -0700

    doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,51 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  let newNode = new Node(val)
+  if (!this.head) {
+    this.head = newNode
+    this.tail = newNode
+  } else {
+    const previousNode = this.tail
+    this.tail.next = newNode
+    this.tail = newNode
+    this.tail.prev = previousNode
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let current = this.head
+  while (current) {
+    if (current.val === val) {
+      if (current === this.head && current === this.tail) {
+        //only one item in list
+        this.head = null
+        this.tail = null
+      } else if (current === this.head) {
+        //middle of the list
+        this.head = this.head.next
+        this.head.prev = null
+      } else if (current === this.tail) {
+        //last item on the list
+        this.tail = this.tail.prev
+        this.tail.next = null
+      } else {
+        current.prev.next = current.next
+        current.next.prev = current.prev
+      }
+    }
+    current = current.next
+  }
 };
+
+let list = new LinkedList()
+list.add('hi')
+list.add('hello')
+list.add('HELLO!23')
+list.remove('hello')
+console.log(list)
 
 module.exports = LinkedList;
