e7224617008e019e6cc0a4f1f4b098cab3781cf9
David Sally <davidcsally@gmail.com>
2017-09-29 09:17:40 -0700

    doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,40 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  const newNode = new Node(val)
+  const tail = this.tail
+  newNode.prev = tail
+  tail.next = newNode
+  this.tail = newNode
+
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  // traverse list
+  let curNode = this.head
+  if (curNode.val === val) {
+    const removed = curNode.val
+    this.head = curNode.next
+    return removed
+  }
+
+  while (curNode) {
+    if (curNode.next.val === val) {
+      const removed = curNode.next.val
+      // removed curNode.next
+      curNode.next = curNode.next.next
+
+      // attach curNode.next.next to curNode
+      curNode.next.next.prev = curNode
+      return removed
+    }
+    else curNode = curNode.next
+  }
+
 };
 
 module.exports = LinkedList;
