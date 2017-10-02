76c37f99c3817f5171bd20e42411b905891e2a29
Jon Roach <roach.jc@gmail.com>
2017-10-02 09:30:00 -0700

    Bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+  let end = array.length - 1;
+  while (end > 1) {
+    for (let i = 0; i < end; i += 1) {
+      if (array[i] > array[i + 1]) [array[i], array[i + 1]] = [array[i + 1], array[i]];
+    }
+    end -= 1;
+  }
+  return array;
 }
+
+console.log(bubbleSort([5,9,6,4,3,7,9,1,6,7]));
 
 module.exports = bubbleSort;
