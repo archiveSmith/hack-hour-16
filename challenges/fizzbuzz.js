922357df3bca28dd0017251c011faf6657c5eda5
Balal Zuhair <balalzuhair@balals-mbp.gateway.pace.com>
2017-11-17 12:04:59 -0800

    add comments and edge case

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,15 +17,25 @@
 //                     'fizzbuzz',
 //                     16 ]
 
+// Big O = O(n)
 function fizzbuzz(num) {
-  newArr = [];
-  for (let i= 1; i <= num; i++) {
-    if (i % 3 === 0 && i % 5 === 0) newArr.push("fizzbuzz");
-    else if (i % 3 === 0) newArr.push("fizz");
-    else if (i % 5 === 0) newArr.push("buzz");
-    else newArr.push(i);
+  // check to see if number is less than 1
+  if (num < 1) return ('number needs to be greater than or equal to 1');
+  // create an array variable
+  const result = [];
+  // iterate starting at 1 and ending at NUM
+  for (let i = 1; i <= num; i += 1) {
+    // if the current value is divisable by 15 then push 'fizzbuzz' into array
+    if (i % 15 === 0) result.push('fizzbuz');
+    // else if the current value is divisable by 3 then push 'fizz' into array
+    else if (i % 3 === 0) result.push('fizz');
+    // else if the current value is divisable by 5 then push 'buzz' into the array
+    else if (i % 5 === 0) result.push('buzz');
+    // else push in value
+    else result.push(i);
   }
-  return newArr;
+  // return the array
+  return result;
 }
 
 module.exports = fizzbuzz;
