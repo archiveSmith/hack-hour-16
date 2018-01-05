39cb2459f544623670f739fa641d2ad96550cf14
Balal Zuhair <balalzuhair@balals-mbp.gateway.pace.com>
2018-01-04 18:42:59 -0800

    updated solution and added comments

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,28 +10,39 @@
  */
 
 
- function modemean(array) {
-  let total = array.reduce(function(sum, value) {
-    return sum + value;
-});
-mean = total/array.length;
-let mode = {};
-  array.forEach(function(element) {
-    if (mode[element] === undefined) mode[element] = 1;
-
-    else return mode[element] += 1;
-  });
-  let countHolder = 0;
-  let keyHolder;
-  for (let keys in mode) {
-    if (mode[keys] > countHolder) {
-      countHolder = mode[keys];
-      keyHolder = keys;
+function modemean(array) {
+  // declare variable total = 0
+  let total = 0;
+  // declare variable mode = []
+  let mode = [];
+  // decalre variable cache = {}
+  const cache = {};
+  // declare variable max = 0
+  let max = 0;
+  // loop through array
+  array.forEach((val) => {
+    // add current val to total
+    total += val;
+    // if current val exists in cache then += 1 to cache[val]
+    if (cache[val]) cache[val] += 1;
+    // if not then cache[val] = 1
+    else cache[val] = 1;
+    // if cache[val] === max then push current val to mode
+    if (cache[val] === max) mode.push(val);
+    // if cache[val > max then mode = [current val] and max = cache[val]
+    if (cache[val] > max) {
+      mode = [val];
+      max = cache[val];
     }
-  }
-  mode = keyHolder;
-  if (mean == mode) return true;
-  else return false;
+  });
+  // calculate mean 
+  const mean = Math.floor(total / array.length);
+  // caculate mode
+  mode = Math.max(...mode);
+  // if mean === mode return true
+  if (mean === mode) return true;
+  // else return false
+  return false;
 }
 
 module.exports = modemean;
